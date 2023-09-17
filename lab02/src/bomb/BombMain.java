5fcae1b2b313972e6e556565153be87b3fa88ef0
LaUrrego <lurrego22@gmail.com>
2023-09-17 09:48:40 -0700

    Complete lab 2

--- lab02/src/bomb/BombMain.java
+++ lab02/src/bomb/BombMain.java
@@ -1,9 +1,38 @@
 package bomb;
 
 import common.IntList;
+import java.util.Random;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
 
 public class BombMain {
+
+
+    private static String passwordGenerator(){
+        List<Integer> passwordList = new ArrayList<>();
+        Random r = new Random(1337);
+        LinkedHashSet<Integer> hs = new LinkedHashSet<>();
+
+        while (hs.size() < 100000){
+            hs.add(r.nextInt());
+        }
+        passwordList.addAll(hs);
+        int passwordNeeded = passwordList.get(1338);
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < 1337; i++){
+            result.append(passwordList.get(i) + " ");
+        }
+        result.append("-81201430");
+        return result.toString();
+    }
+
     public static void main(String[] args) {
+
+        String phase2Pass = passwordGenerator();
+
         int phase = 2;
         if (args.length > 0) {
             phase = Integer.parseInt(args[0]);
@@ -11,13 +40,13 @@ public class BombMain {
         // TODO: Find the correct inputs (passwords) to each phase using debugging techniques
         Bomb b = new Bomb();
         if (phase >= 0) {
-            b.phase0("Figure this out. I wonder where the phases are defined...");
+            b.phase0("39291226");
         }
         if (phase >= 1) {
-            b.phase1(null); // Figure this out too
+            b.phase1( IntList.of(0,9,3,0,8)); // Figure this out too
         }
         if (phase >= 2) {
-            b.phase2("Figure this out. I wonder where the phases are defined...");
+            b.phase2(phase2Pass);
         }
     }
 }
